549cdfbf042e3279658e66aaf856eb4910297f1f
jrquadros <quadrosjunioradits@gmail.com>
2019-04-07 02:06:37 -0300

    location event listener added

--- web/src/App.js
+++ web/src/App.js
@@ -1,40 +1,24 @@
 import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  const [ repositories, setRepositories ] = useState([]);
-
-  useEffect(async () => {
-    const response = await fetch('https://api.github.com/users/jrquadros/repos');
-    const data = await response.json();
-
-    setRepositories(data);
+  const [location, setLocation] = useState({});
+  
+  useEffect(() => {
+    navigator.geolocation.watchPosition(handlePositionReceived)
   }, []);
 
-  useEffect(() => {
-    const filtered = repositories.filter(repo => repo.favorite);
-
-    document.title = `Você tem ${filtered.length} favoritos`
-  }, [repositories]);
-
-  function handleFavorit(id) {
-    const newRepositories = repositories.map(repo => {
-      return repo.id === id ? { ...repo, favorite: !repo.favorite} : repo
-    });
-
-    setRepositories(newRepositories);
+  function handlePositionReceived({coords}) {
+    const {latitude, longitude } = coords;
+    setLocation({ latitude, longitude })
+    console.log(coords)
   }
 
   return (
 
-    <ul>
-     {repositories.map(repo => (
-        <li key={repo.id}>
-          {repo.name}
-          {repo.favorite && <span>(Favorito)</span>}
-          <button onClick={() => handleFavorit(repo.id)}>Favoritar</button>
-        </li>
-     ))}
-    </ul>
+    <>
+    Latitude: {location.latitude} <br />
+    Longitude: {location.longitude}
+    </>
 
   )
 }
