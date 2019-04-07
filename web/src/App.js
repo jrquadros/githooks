989e6a6c3d35db51b05b2589a88c68307d79b454
jrquadros <quadrosjunioradits@gmail.com>
2019-04-07 01:47:02 -0300

    get user and favorite button

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        githooks
-      </div>
-    );
+export default function App() {
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch('https://api.github.com/users/jrquadros/repos');
+    const data = await response.json();
+
+    setRepositories(data);
+  }, []);
+
+  function handleFavorit(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: true} : repo
+    });
+
+    setRepositories(newRepositories);
   }
-}
 
-export default App;
+  return (
+
+    <ul>
+     {repositories.map(repo => (
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>(Favorito)</span>}
+          <button onClick={() => handleFavorit(repo.id)}>Favoritar</button>
+        </li>
+     ))}
+    </ul>
+
+  )
+}
